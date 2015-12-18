0f442dbffebffb670e32d27aa501a61b3d3f4f7f
Ross Burrows <ross.h.burrows@gmail.com>
2015-12-18 12:42:28 -0600

    init material for Ch2.

---
+++ CH2/convert.py
@@ -0,0 +1,7 @@
+# convert celsius to fahrenheit 
+def conCtoF():
+        print("This program function converts Celsius to degrees Fahrenheit.")
+        cs = eval(input("Enter the Celsius Temperature: "))
+        ft = (9/5)*cs + 32
+        print("Temperature = ", ft, "degrees Fahrenheit")
+conCtoF()
